5bf2fe2dbdd87b17ae2a5f553c4d937c9a26f3cf
javeed512 <javeedmohammedjaveed@gmail.com>
2022-02-28 17:18:25 +0530

    Queue Demo

---
+++ QueueDemo/src/com/mphasis/queue/CustomeQueue.java
@@ -0,0 +1,22 @@
+package com.mphasis.queue;
+
+public class CustomeQueue {
+
+	public static void main(String[] args) {
+
+		LinkedList  queue =		new LinkedList();
+		
+				LinkedList list = new LinkedList();
+		
+				queue.insert(list, 10);
+				queue.insert(list,20);
+				queue.insert(list, 30);
+				
+				
+				queue.printList(list);
+			
+				
+
+	}
+
+}
